348cf4b77c0a14facedaaef96a88d451b5cc3ce2
Jon Kunkee <jkunkee@ntdev.microsoft.com>
2020-03-29 22:57:54 -0700

    Refactor alarm state machine

--- main/alarm.c
+++ main/alarm.c
@@ -21,7 +21,6 @@ typedef enum _alarm_wait_task_state_t
     running,
     sleep_mode_start,
     sleep_mode_run,
-    disabled,
 } alarm_wait_task_state_t;
 
 #define ALARM_STOP_BIT BIT0
@@ -38,12 +37,13 @@ typedef enum _alarm_wait_task_state_t
     | SLEEP_STOP_BIT \
     )
 
-static alarm_wait_task_state_t alarm_current_state;
 static EventGroupHandle_t alarm_event_group;
 static TaskHandle_t alarm_task;
 
 void alarm_task_func(void* param)
 {
+    alarm_wait_task_state_t alarm_current_state = initializing;
+    alarm_wait_task_state_t alarm_next_state = initializing;
     time_t prev_now = 0;
     time_t now = 0;
     uint32_t alarm_enabled_raw = 0;
@@ -62,13 +62,127 @@ void alarm_task_func(void* param)
             pdFALSE,
             1000 / portTICK_PERIOD_MS);
 
-        time(&now);
-        // TODO: Get more consistent behavior by separating state transitions from state actions
+        // Encode state transitions separate from state actions
+        // Note that this design will swallow simultaneously set bits.
+
+        alarm_next_state = alarm_current_state;
+
         switch (alarm_current_state)
         {
         case initializing:
-            // wait for first configuration event
+            // wait for the first alarm configuration event
             if (bits & ALARM_RECONFIG_BIT)
+            {
+                alarm_next_state = configuring;
+            }
+            break;
+        case configuring:
+            alarm_next_state = waiting;
+            break;
+        case waiting:
+            if (bits & ALARM_RECONFIG_BIT)
+            {
+                alarm_next_state = configuring;
+            }
+            else if (bits & SLEEP_START_BIT)
+            {
+                alarm_next_state = sleep_mode_start;
+            }
+            else if (alarm_enabled)
+            {
+                // calculate when alarm should happen
+                struct tm prev_local_now;
+                localtime_r(&prev_now, &prev_local_now);
+                struct tm local_now;
+                localtime_r(&now, &local_now);
+                // now is guaranteed to be between these three times
+                // today alarm time
+                struct tm today_alarm_local_time = local_now;
+                today_alarm_local_time.tm_hour = alarm_hour;
+                today_alarm_local_time.tm_min = alarm_minute;
+                today_alarm_local_time.tm_sec = 0;
+                // https://linux.die.net/man/3/mktime
+                time_t today_alarm_time = mktime(&today_alarm_local_time);
+                // yesterday alarm time
+                struct tm yesterday_alarm_local_time = local_now;
+                yesterday_alarm_local_time.tm_hour = alarm_hour;
+                yesterday_alarm_local_time.tm_min = alarm_minute;
+                yesterday_alarm_local_time.tm_sec = 0;
+                yesterday_alarm_local_time.tm_yday -= 1;
+                time_t yesterday_alarm_time = mktime(&yesterday_alarm_local_time);
+                // tomorrow alarm time
+                struct tm tomorrow_alarm_local_time = local_now;
+                tomorrow_alarm_local_time.tm_hour = alarm_hour;
+                tomorrow_alarm_local_time.tm_min = alarm_minute;
+                tomorrow_alarm_local_time.tm_sec = 0;
+                tomorrow_alarm_local_time.tm_yday -= 1;
+                time_t tomorrow_alarm_time = mktime(&tomorrow_alarm_local_time);
+                // if it was during last wait, trigger alarm
+                // Checking all three makes this resilient to near-midnight alarm times
+                if ((prev_now <= yesterday_alarm_time && yesterday_alarm_time <= now) ||
+                    (prev_now <= today_alarm_time && today_alarm_time <= now) ||
+                    (prev_now <= tomorrow_alarm_time && tomorrow_alarm_time <= now) )
+                {
+                    ESP_LOGI(TAG, "Alarm triggered at Unix Epoch %ld", now);
+                    alarm_next_state = running;
+                }
+            }
+            break;
+        case snoozing:
+            if (bits & ALARM_STOP_BIT)
+            {
+                alarm_next_state = waiting;
+            }
+            else if (bits & ALARM_RECONFIG_BIT)
+            {
+                alarm_next_state = configuring;
+            }
+            else
+            {
+                // TODO
+                alarm_next_state = running;
+                // Alarm snooze time transition detection
+            }
+            break;
+        case running:
+            if (bits & ALARM_STOP_BIT)
+            {
+                alarm_next_state = waiting;
+            }
+            else if (bits & ALARM_RECONFIG_BIT)
+            {
+                alarm_next_state = configuring;
+            }
+            else if (bits & ALARM_SNOOZE_BIT)
+            {
+                alarm_next_state = snoozing;
+            }
+            break;
+        case sleep_mode_start:
+            alarm_next_state = sleep_mode_run;
+            break;
+        case sleep_mode_run:
+            if (bits & SLEEP_STOP_BIT || sleep_mode_step_count >= FADE_STEP_COUNT)
+            {
+                alarm_next_state = waiting;
+            }
+            else if (bits & ALARM_RECONFIG_BIT)
+            {
+                alarm_next_state = configuring;
+            }
+            break;
+        default:
+            ESP_LOGE(TAG, "Invalid enum value seen! %d", alarm_current_state);
+        }
+
+        // per-state actions
+        time(&now);
+
+        switch (alarm_current_state)
+        {
+        case initializing:
+            // On transition out of this state, set status LED
+            if (alarm_current_state != alarm_next_state)
             {
                 led_set_status_indicator(led_status_alarm, LED_STATUS_COLOR_SUCCESS);
             }
@@ -81,111 +195,36 @@ void alarm_task_func(void* param)
             ESP_ERROR_CHECK( get_setting("alarm_snooze_interval_min", &alarm_snooze_interval_min) );
             ESP_ERROR_CHECK( get_setting("alarm_led_pattern", &alarm_led_pattern_raw) );
             alarm_pattern = (led_pattern_t)alarm_led_pattern_raw;
-            if (alarm_enabled)
-            {
-                alarm_current_state = waiting;
-            }
-            else
-            {
-                alarm_current_state = disabled;
-            }
             led_run_sync(led_pattern_blank);
             break;
         case waiting:
-            ESP_LOGI(TAG, "prev_now: %ld, now: %ld", prev_now, now);
-            // calculate when alarm should happen
-            struct tm prev_local_now;
-            localtime_r(&prev_now, &prev_local_now);
-            struct tm local_now;
-            localtime_r(&now, &local_now);
-            // now is guaranteed to be between these three times
-            // today alarm time
-            struct tm today_alarm_local_time = local_now;
-            today_alarm_local_time.tm_hour = alarm_hour;
-            today_alarm_local_time.tm_min = alarm_minute;
-            today_alarm_local_time.tm_sec = 0;
-            // https://linux.die.net/man/3/mktime
-            time_t today_alarm_time = mktime(&today_alarm_local_time);
-            // yesterday alarm time
-            struct tm yesterday_alarm_local_time = local_now;
-            yesterday_alarm_local_time.tm_hour = alarm_hour;
-            yesterday_alarm_local_time.tm_min = alarm_minute;
-            yesterday_alarm_local_time.tm_sec = 0;
-            yesterday_alarm_local_time.tm_yday -= 1;
-            time_t yesterday_alarm_time = mktime(&yesterday_alarm_local_time);
-            // tomorrow alarm time
-            struct tm tomorrow_alarm_local_time = local_now;
-            tomorrow_alarm_local_time.tm_hour = alarm_hour;
-            tomorrow_alarm_local_time.tm_min = alarm_minute;
-            tomorrow_alarm_local_time.tm_sec = 0;
-            tomorrow_alarm_local_time.tm_yday -= 1;
-            time_t tomorrow_alarm_time = mktime(&tomorrow_alarm_local_time);
-            // if it was during last wait, trigger alarm
-            // Checking all three makes this resilient to near-midnight alarm times
-            if ((prev_now <= yesterday_alarm_time && yesterday_alarm_time <= now) ||
-                (prev_now <= today_alarm_time && today_alarm_time <= now) ||
-                (prev_now <= tomorrow_alarm_time && tomorrow_alarm_time <= now) )
-            {
-                ESP_LOGI(TAG, "Alarm triggered at Unix Epoch %ld", now);
-                alarm_current_state = running;
-            }
+            // When control comes back, we'll compare them and now to see if the alarm should fire.
+            prev_now = now;
             break;
         case snoozing:
             led_run_sync(led_pattern_blank);
-            if (bits & ALARM_STOP_BIT)
-            {
-                alarm_current_state = waiting;
-            }
             break;
         case running:
             led_run_sync(led_pattern_blank);
             led_run_sync(alarm_pattern);
-            if (bits & ALARM_SNOOZE_BIT)
-            {
-                alarm_current_state = snoozing;
-            }
-            else if (bits & ALARM_STOP_BIT)
-            {
-                alarm_current_state = waiting;
-            }
             break;
         case sleep_mode_start:
             sleep_mode_step_count = 0;
             led_run_sync(led_pattern_fade_start);
             break;
         case sleep_mode_run:
-            if (sleep_mode_step_count < FADE_STEP_COUNT && !(bits & SLEEP_STOP_BIT))
+            if (alarm_current_state == alarm_next_state)
             {
                 sleep_mode_step_count++;
                 led_run_sync(led_pattern_fade_step);
             }
-            else
-            {
-                alarm_current_state = configuring;
-            }
-            break;
-        case disabled:
-            // wait for reconfiguration
             break;
         default:
             ESP_LOGE(TAG, "Invalid enum value seen! %d", alarm_current_state);
         }
 
-        // When control comes back, we'll compare then and now to see if the alarm should fire.
-        prev_now = now;
-
-        // Start the sleep pattern if the alarm is not going off. Alarm will be ignored during this time.
-        if (bits & SLEEP_START_BIT && (alarm_current_state == waiting || alarm_current_state == disabled))
-        {
-            alarm_current_state = sleep_mode_start;
-        }
-
-        // In all states, the reconfig bit trumps all.
-        if (bits & ALARM_RECONFIG_BIT)
-        {
-            alarm_current_state = configuring;
-        }
-    }
+        alarm_current_state = alarm_next_state;
+    } // while pdTRUE
 }
 
 void alarm_system_time_or_settings_changed()
@@ -211,7 +250,6 @@ bool is_sleep_running();
 
 esp_err_t init_alarm()
 {
-    alarm_current_state = initializing;
     alarm_event_group = xEventGroupCreate();
     xTaskCreate(alarm_task_func, "time_check_task Task", 4*1024, NULL, 1, &alarm_task);
 
