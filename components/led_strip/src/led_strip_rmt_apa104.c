73270347da73b7c38502da362f7b74c3407ef8fd
Jon Kunkee <jkunkee@ntdev.microsoft.com>
2020-04-09 23:47:10 -0700

    Add gamma correction based on http://rgb-123.com/ws2812-color-output/

--- components/led_strip/src/led_strip_rmt_apa104.c
+++ components/led_strip/src/led_strip_rmt_apa104.c
@@ -42,6 +42,29 @@ static uint32_t apa104_t1h_ticks = 0;
 static uint32_t apa104_t0l_ticks = 0;
 static uint32_t apa104_t1l_ticks = 0;
 
+// Gamma correction (http://rgb-123.com/ws2812-color-output/)
+uint8_t gamma_lut[256] = {
+  0,  0,  0,  0,   0,  0,  0,  0,   0,  0,  0,  0,   0,  0,  0,  0,
+  0,  0,  0,  0,   0,  0,  1,  1,   1,  1,  1,  1,   1,  2,  2,  2,
+  2,  2,  2,  3,   3,  3,  3,  3,   4,  4,  4,  4,   5,  5,  5,  5,
+  6,  6,  6,  7,   7,  7,  8,  8,   8,  9,  9,  9,  10, 10, 11, 11,
+
+ 11, 12, 12, 13,  13, 13, 14, 14,  15, 15, 16, 16,  17, 17, 18, 18,
+ 19, 19, 20, 21,  21, 22, 22, 23,  23, 24, 25, 25,  26, 27, 27, 28,
+ 29, 29, 30, 31,  31, 32, 33, 34,  34, 35, 36, 37,  37, 38, 39, 40,
+ 40, 41, 42, 43,  44, 45, 46, 46,  47, 48, 49, 50,  51, 52, 53, 54,
+
+ 55, 56, 57, 58,  59, 60, 61, 62,  63, 64, 65, 66,  67, 68, 69, 70,
+ 71, 72, 73, 74,  76, 77, 78, 79,  80, 81, 83, 84,  85, 86, 88, 89,
+ 90, 91, 93, 94,  95, 96, 98, 99, 100,102,103,104, 106,107,109,110,
+111,113,114,116, 117,119,120,121, 123,124,126,128, 129,131,132,134,
+
+135,137,138,140, 142,143,145,146, 148,150,151,153, 155,157,158,160,
+162,163,165,167, 169,170,172,174, 176,178,179,181, 183,185,187,189,
+191,193,194,196, 198,200,202,204, 206,208,210,212, 214,216,218,220,
+222,224,227,229, 231,233,235,237, 239,241,244,246, 248,250,252,255
+};
+
 typedef struct {
     led_strip_t parent;
     rmt_channel_t rmt_channel;
@@ -100,9 +123,9 @@ static esp_err_t apa104_set_pixel(led_strip_t *strip, uint32_t index, uint32_t r
     STRIP_CHECK(index < apa104->strip_len, "index out of the maximum number of leds", err, ESP_ERR_INVALID_ARG);
     uint32_t start = index * 3;
     // In thr order of GRB
-    apa104->buffer[start + 0] = green & 0xFF;
-    apa104->buffer[start + 1] = red & 0xFF;
-    apa104->buffer[start + 2] = blue & 0xFF;
+    apa104->buffer[start + 0] = gamma_lut[green & 0xFF];
+    apa104->buffer[start + 1] = gamma_lut[red & 0xFF];
+    apa104->buffer[start + 2] = gamma_lut[blue & 0xFF];
     return ESP_OK;
 err:
     return ret;
